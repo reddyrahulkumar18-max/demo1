90cc9d2cbe25927bc86c7f3ebc7c4fe0eda269b5
S RAHUL KUMAR REDDY <reddyrahulkumar18@gmail.com>
2026-03-25 13:14:11 +0530

    Project Structure

---
+++ uc 1.java
@@ -0,0 +1,5 @@
+public class HelloApp {
+    public static void main(String[] args){
+        system.out.print.ln("Hello,World !");
+    }
+}
---
+++ uc2.java
@@ -0,0 +1,9 @@
+public class HelloApp {
+    public static void main(String[] args){
+            string name = args[0];
+            System.out.println("Hello, "+ name +"!");
+        }
+    }
+    
+
+}
